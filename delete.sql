9e31e364770e58407e0909a087e3b041909d4ca7
Aman Pratap Singh <63160825+Dr-Groot@users.noreply.github.com>
2021-05-27 20:24:23 +0530

    Update delete.sql

--- delete.sql
+++ delete.sql
@@ -1,1 +1,14 @@
 /* The DELETE statement is used to delete existing records in a table. */
+
+/* DELETE SYNTAX */
+DELETE FROM table_name
+WHERE condition;
+
+
+/* ------------EXAMPLE-------------*/
+/* If there is a Table name as "CustomerComplaint" having CustomerID, CustomerName, ProductName
+Issue, City, PostalCode and Country columns which are having data */
+
+/* SQL statement deletes the customer "Philip" from the “CustomerComplaint" table: */
+DELETE FROM CustomerComplaint 
+WHERE CustomerName=‘Philip';
