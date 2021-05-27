55c6395e044da8acb3b5e0913477f4dc151fc820
Aman Pratap Singh <63160825+Dr-Groot@users.noreply.github.com>
2021-05-27 15:36:20 +0530

    Create synatax.sql

---
+++ synatax.sql
@@ -0,0 +1,4 @@
+/*A database most often contains one or more tables. Each table is identified by a name (e.g. "Customers" or "Orders"). 
+  Tables contain records (rows) with data.*/
+  SELECT * FROM TABLE_NAME;
+/* Line 3 will allow all the element of the table to be displayed */
