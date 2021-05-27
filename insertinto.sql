464b0c7e7491e8187446c737a8b5a26a87ef4623
Aman Pratap Singh <63160825+Dr-Groot@users.noreply.github.com>
2021-05-27 19:42:27 +0530

    Create insertinto.sql

---
+++ insertinto.sql
@@ -0,0 +1,12 @@
+/* To insert new record we use INSERT INTO Statement */
+
+/* INSERT INTO SYNTAX 
+FIRST WAY by specifying column names and values:-  */
+
+INSERT INTO table_name (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...);
+
+/* SECOND WAY by giving table name and values */
+INSERT INTO table_name
+VALUES (value1, value2, value3, ...);
+
