bc0cae9531f0c7f2fbc8bbb03e6775fbc57a03fa
Aman Pratap Singh <63160825+Dr-Groot@users.noreply.github.com>
2021-05-27 20:13:21 +0530

    UPDATE SQL

---
+++ update.sql
@@ -0,0 +1,27 @@
+/* The UPDATE statement is used to modify the existing records in a table.*/
+
+/* UPDATE SYNTAX */
+UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE condition;
+
+/* While using UPDATE be carefull as it can update all the record in the table so we use WHERE Clause */
+
+
+/* ------------EXAMPLE-------------*/
+/* If there is a Table name as "CustomerComplaint" having CustomerID, CustomerName, ProductName
+Issue, City, PostalCode and Country columns */
+
+/* SQL statement updates the first customer (CustomerID = 1) with a new contact person and a new city.*/
+UPDATE CustomerComplaint
+SET CustomerName = 'CHANGE1', City= ‘CHANGE2'
+WHERE CustomerID = 1;
+
+/* UPDATING MULTIPLE RECORD: update the CustomerName to "JAMES" for all records where country is "India": */
+UPDATE CustomerComplaint
+SET ContactName='JAMES'
+WHERE Country=‘India';
+
+/* UPDATING WITHOUT WHERE will omit all the record to be changed */
+UPDATE CustomerComplaint
+SET CustomerName=‘hello';
