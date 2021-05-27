54a1fbe7b928f5519e03797fb5856b8ced32f89d
Aman Pratap Singh <63160825+Dr-Groot@users.noreply.github.com>
2021-05-27 20:28:27 +0530

    Update delete.sql

--- delete.sql
+++ delete.sql
@@ -4,6 +4,9 @@
 DELETE FROM table_name
 WHERE condition;
 
+/* Be careful when deleting records in a table! Notice the WHERE clause in the DELETE statement. The WHERE clause 
+specifies which record(s) should be deleted. If you omit the WHERE clause, all records in the table will be deleted!*/
+
 
 /* ------------EXAMPLE-------------*/
 /* If there is a Table name as "CustomerComplaint" having CustomerID, CustomerName, ProductName
@@ -11,4 +14,8 @@ Issue, City, PostalCode and Country columns which are having data */
 
 /* SQL statement deletes the customer "Philip" from the “CustomerComplaint" table: */
 DELETE FROM CustomerComplaint 
-WHERE CustomerName=‘Philip';
+WHERE CustomerName=‘Philip;
+
+/* DELETE all Record from CustomerComplaint Table*/
+DELETE FROM CustomerComplaint;
+/* While running this query it will only delete records not the table */
