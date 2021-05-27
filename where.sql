7d2acac274e6d29faff90e5346697ce815d55f5f
Aman Pratap Singh <63160825+Dr-Groot@users.noreply.github.com>
2021-05-27 18:36:52 +0530

    Create where.sql

---
+++ where.sql
@@ -0,0 +1,9 @@
+/* For filtering record we use Where Clause, extracting information that fulfill a specified condition. */
+
+/* WHERE SYNTAX */
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition;
+
+/* WHERE is used with SELECT, UPDATE, DELETE, etc. */
+
