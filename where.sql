a573331a215d07dd4920d50e74446f3fcaaf57e0
Aman Pratap Singh <63160825+Dr-Groot@users.noreply.github.com>
2021-05-27 18:40:01 +0530

    Where SQL

--- where.sql
+++ where.sql
@@ -7,3 +7,14 @@ WHERE condition;
 
 /* WHERE is used with SELECT, UPDATE, DELETE, etc. */
 
+/* ------------EXAMPLE-------------*/
+/* If there is a Table name as "CustomerComplaint" having CustomerID, CustomerName, ProductName
+Issue, City, PostalCode and Country columns which are having data */
+
+/* If we want to select all customers from India */
+SELECT * FROM Customers
+WHERE Country='India';
+
+/* If we want to select customer which is having customerID 12 */
+SELECT * FROM Customers
+WHERE CustomerID=12;
