43330e8c846ae5ea3d4619ecab04bfdf3351d967
Aman Pratap Singh <63160825+Dr-Groot@users.noreply.github.com>
2021-05-27 20:57:01 +0530

    Create minmax.sql

---
+++ minmax.sql
@@ -0,0 +1,12 @@
+/* The MIN() function returns the smallest value of the selected column.
+  The MAX() function returns the largest value of the selected column. */
+  
+/* MIN() SYNTAX */
+SELECT MIN(column_name)
+FROM table_name
+WHERE condition;
+
+/* MAX() SYNTAX */
+SELECT MAX(column_name)
+FROM table_name
+WHERE condition;
